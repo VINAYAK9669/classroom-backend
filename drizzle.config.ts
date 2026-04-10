490490f5bdab861db952ad7ea2d086a0af4c0863
VINAYAK9669 <vinayak.k@uexcelerate.com>
2026-04-10 08:50:31 +0530

    Connected to Db, Wrote down the schema, correct the versions

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set in .env file");
+}
+
+export default {
+  schema: "./src/db/schema.ts/app.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+};
