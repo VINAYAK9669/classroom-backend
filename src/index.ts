dbab439e660b3d73b081e4e9bc60aa0dc12dd9c5
VINAYAK9669 <vinayak.k@uexcelerate.com>
2026-04-13 07:42:35 +0530

    Backend changes,

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import express from "express";
+import subjectsRouter from "./routes/subjects.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
+
+// HANDLE CORS
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
 app.get("/", (_req, res) => {
   res.json({ message: "Backend server is running." });
 });
+
+app.use("/api/subjects", subjectsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
